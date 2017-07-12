e99b4b77f64feec41670f884c50c8c25e516fa46
Offain <fabian.leutenegger@bluewin.ch>
2017-07-12 18:50:42 +0200

    desable BLOCK_BASED_OTA and BUILD NINJA (reverted from commit 40ab72cddb16b06ab0ef7a89e13e5778fed19e4c)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -226,12 +226,6 @@ WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
 WIFI_DRIVER_MODULE_NAME := "wlan"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
-# BLOCK_BASED_OTA
-BLOCK_BASED_OTA := true
-
-# DISABLED BUILD NINJA
-USE_NINJA := false
-
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
